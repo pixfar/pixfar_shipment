4001dfa2079508136bd76d7c46c0d357e2f36516
Shaid Azmin <excel.azmin@gmail.com>
2024-06-29 08:29:24 +0000

    add missing doctype

---
+++ pixfar_shipment/pixfar_shipment/doctype/shipment_settings/shipment_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Pixfar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shipment Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
